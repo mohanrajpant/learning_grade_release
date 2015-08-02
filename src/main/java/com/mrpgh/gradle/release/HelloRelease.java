dc52cefdec88a19705aceee46c2255c4a85a030c
mohanraj.pant <mohanraj.pant@gmail.com>
2015-08-02 13:44:38 +0100

    [LAK-000|MP] Learning about gradle release plugin

---
+++ src/main/java/com/mrpgh/gradle/release/HelloRelease.java
@@ -0,0 +1,9 @@
+package com.mrpgh.gradle.release;
+
+public class HelloRelease {
+
+    public static void main(String [] args) {
+        System.out.println("Welcome to Gradle Release");
+    }
+
+}
